5bbcb8a17a015e70e099812cb071de37d3920ef2
Mehul Yadav <mehulyadav432@gmail.com>
2024-01-28 18:59:41 +0530

    classes and objects added

---
+++ classesandobjects.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// class is an template , blueprint of a data type (this book class is specification of a book we are going to make mf...)
+class Book
+{
+public:
+    string title;
+    string author;
+    int pages;
+};
+
+int main()
+{
+
+    // object is an actual instance of a speification..
+    Book book1 ;
+    book1.title = "dostovyesky";
+    book1.pages = 69;
+
+    Book book2 ;
+    book2.title = "brothers karmazov";
+    book2.author = "idk";
+    book2.author = "i still dont know";
+     cout << book1.title << endl;
+     cout << book2.author << endl;
+
+
+
+    return 0;
+}
