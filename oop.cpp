781eaf3ac79dfedb4817268282e77653ecc2779f
Mehul Yadav <mehulyadav432@gmail.com>
2024-02-01 21:12:55 +0530

    oop update

--- oop.cpp
+++ oop.cpp
@@ -3,27 +3,34 @@ using namespace std;
 
 class employee
 {
-  public:
+public:
     string name;
     string company;
     int age;
 
-    void introduceyourself() {
+    void introduceyourself()
+    {
         cout << "Name-" << name << endl;
         cout << "Company--" << company << endl;
         cout << "age-" << age << endl;
     }
+    employee(string Name, string Company, int Age)
+    {
+        name = Name;
+        company = Company;
+        age = Age;
+    };
 };
 
 int main()
 {
-    employee employee1 ;
+    employee employee1 = employee("saldina", "code beauty", 25);
     employee1.company = "MY corp";
     employee1.name = "M";
     employee1.age = 21;
     employee1.introduceyourself();
 
-    employee employee2 ;
+    employee employee2 = employee("john", "amazon", 35);
     employee2.name = "Anu";
     employee2.introduceyourself();
 }
