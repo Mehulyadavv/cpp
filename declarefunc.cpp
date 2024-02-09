16c46a2e9249c4d81bea633822ba7f9c5c4e0261
Mehul Yadav <mehulyadav432@gmail.com>
2024-02-09 23:50:22 +0530

    lol

---
+++ declarefunc.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+//declare function
+int lolfunc();
+int main(){
+    //invoke func
+    lolfunc();
+    return 0;
+
+}
+//implement function
+int lolfunc(){
+    cout << "hiya" << endl;
+    return 0 ;
+}
---
+++ returnvalueofafunc.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+int funclol(){
+    cout<<"hiya"<<endl;
+    return 0 ;
+}
+    int main()
+    {
+        return funclol();
+    }
